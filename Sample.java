8f5262d5917943abaac9bc94d9f6d0ea7c537f29
praiseshalom27 <praiseshalomshaji27@gmail.com>
2021-11-13 20:26:05 +0530

    added file

---
+++ Sample.java
@@ -0,0 +1,11 @@
+class Sample
+{
+
+	public static void main (String args[])
+	{
+		System.out.println("Hello World");
+		//System.out.println("Sample Program");
+		System.out.print("Sample Program1");
+	}
+}
+ 
